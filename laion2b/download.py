1fb115e198cc23248e514aeffd1e39fc2102e841
Ken Børge Viktil <599468+kenjiqq@users.noreply.github.com>
2023-05-18 17:09:59 +0200

    update laion urls

--- laion2b/download.py
+++ laion2b/download.py
@@ -2,13 +2,13 @@ from img2dataset import download
 import shutil
 import os
 
-output_dir = os.path.abspath("laion2b-en/dataset")
+output_dir = os.path.abspath("laion2b/dataset")
 
 if __name__ == '__main__':
     download(
         processes_count=8,
         thread_count=16,
-        url_list="laion2b-en/parquet/dataset.parquet",
+        url_list="laion2b/parquet/dataset.parquet",
         image_size=256,
         output_folder=output_dir,
         output_format="webdataset",
